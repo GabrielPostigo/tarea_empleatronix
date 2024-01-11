b1211b00258b17b1f38faf3ce6d3e3259b452130
Postigo Rando <g.postigo.rando@accenture.com>
2024-01-11 13:12:46 +0100

    Archivo requirement reparado

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM python:3.8
-RUN pip install numpy pandas streamlit matplotlip
+RUN pip install numpy pandas streamlit matplotlib seaborn
 COPY src/* /app/
 WORKDIR /app
 ENTRYPOINT [ "streamlit", "run", "app.py", "--server.port=8501", "--server.address=0.0.0.0"]
--- src/app.py
+++ src/app.py
@@ -10,4 +10,6 @@ datos_set = pd.read_csv("employees2.csv")
 # Ponemos el título
 st.title('EMPLEATRONIX')
 st.write('Todos los datos sobre los empleados en una aplicación.')
-st.write(datos_set)
+st.write(datos_set) # mostramos el dataframe
+
+st.divider()    # es la linea separadora
